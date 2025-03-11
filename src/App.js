8c109c16ecfb5220b4c8e922f302c896ff9eb901
Daniele Karina dos Santos <danimistron@gmail.com>
2025-03-11 05:29:08 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import Input from './components/Input';
+import Button from './components/Button';
+
+import { Container, Content, Row } from './styles';
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
+
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+    setFirstNumber('0')
+    setOperation('')
+  }
+
+  const handleAddNumber = (num) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
+  }
+
+  const handleSumNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('+')
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+  }
+
+  const handleMinusNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('-')
+    } else {
+      const minus = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(minus))
+      setOperation('')
+    }
+  }
+
+  const handleMultiplyNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('*')
+    } else {
+      const multiply = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(multiply))
+      setOperation('')
+    }
+  }
+
+  const handleDivisionNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('/')
+    } else {
+      const division = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(division))
+      setOperation('')
+    }
+  }
+
+  const handlePercentageNumbers = () => {
+    if (currentNumber === '0' || currentNumber === '') return;
+
+    const percentage = Number(currentNumber) / 100;
+    setCurrentNumber(String(percentage));
+    setOperation('');
+  };
+
+
+  const handleEquals = () => {
+    if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      switch (operation) {
+        case '+':
+          handleSumNumbers();
+          break;
+        case '-':
+          handleMinusNumbers();
+          break;
+        case '*':
+          handleMultiplyNumbers();
+          break;
+        case '/':
+          handleDivisionNumbers();
+          break;
+        case '%':
+          handlePercentageNumbers();
+          break;
+        default:
+          break;
+      }
+
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber} />
+        <Row>
+          <Button label="C" onClick={handleOnClear} />
+          <Button label="%" onClick={handlePercentageNumbers} />
+          <Button label="C" onClick={handleOnClear} />
+          <Button label="/" onClick={handleDivisionNumbers} />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')} />
+          <Button label="8" onClick={() => handleAddNumber('8')} />
+          <Button label="9" onClick={() => handleAddNumber('9')} />
+          <Button label="*" onClick={handleMultiplyNumbers} />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')} />
+          <Button label="5" onClick={() => handleAddNumber('5')} />
+          <Button label="6" onClick={() => handleAddNumber('6')} />
+          <Button label="-" onClick={handleMinusNumbers} />
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')} />
+          <Button label="2" onClick={() => handleAddNumber('2')} />
+          <Button label="3" onClick={() => handleAddNumber('3')} />
+          <Button label="+" onClick={handleSumNumbers} />
+        </Row>
+        <Row>
+          <Button label="." onClick={() => handleAddNumber('.')} />
+          <Button label="0" onClick={() => handleAddNumber('0')} />
+          <Button label="." onClick={() => handleAddNumber('.')} />
+          <Button label="=" onClick={handleEquals} />
+        </Row>
+      </Content>
+    </Container >
   );
 }
 
---
+++ src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div `
+    width: 100%;
+    height: 75px;
+    background-color:#441a42;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size:24px;
+    font-family: 'Roboto';
+
+    input {
+    width: 100%;
+    height: 75px;
+    padding: 0 10px;
+    background-color:#441a42;
+    border:0;
+    text-align: right;
+
+    font-size:24px;
+    font-family: 'Roboto';
+    color: #FFFFFF
+    }
+`
